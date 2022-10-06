a8cb8437f89b4b90e962e07f1d62ae3a8b295170
Vikram Karthikeyan <viki.kma085@gmail.com>
2022-10-06 21:55:27 +0530

    Create data_cleaning.py
    Perform data cleaning activities

---
+++ src/data_cleaning.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+def data_cleaning(df):
+
+    # Add flag column for missing values
+    for col in df.columns:
+        if df[col].isnull().sum() > 0:
+            df[col+"_missing_flag"] = df[col].isnull()
+
+    # Drop the columns where one category contains more than 90% values
+    drop_cols = set()
+    for col in df.columns:
+        missing_share = df[col].isnull().sum()/df.shape[0]
+        if missing_share > 0.9:
+            drop_cols.add(col)
+
+    good_cols = [col for col in df.columns if col not in drop_cols]
+
+    # Drop the columns which have only one unique value    
+    for col in good_cols:
+        unique_value = df[col].nunique()
+        if unique_value == 1:
+            drop_cols.add(col)
+
+    good_cols = [col for col in df.columns if col not in drop_cols]
+
+    return df[good_cols]
+
+if __name__ == "__main__":
+
+    df_id = pd.read_csv("input/train_identity.csv")
+    df_tran = pd.read_csv("input/train_transaction.csv")
+    df = df_tran.merge(df_id, how='left', on='TransactionID')
+    data = data_cleaning(df)
+
+    print("Preprocessing Successfull!!!")
